828258145c1b9a093803eab6f7285b887c2df647
John M. Stassen <john.stassen@gmail.com>
2021-08-02 15:11:41 -0700

    commit tree 54678b9311f45536541d4445ebf438c89999839d

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+function map(srcArray, fn) {
+    let newArray = []
+    for (let i = 0; i<srcArray.length; i++) {
+        newArray.push(fn(srcArray[i]))
+    }
+    return newArray
+}
+
+function reduce(srcArray, fn, startValue) {
+    let r = (!!startValue) ? startValue : srcArray[0]
+    let i = (!!startValue) ? 0 : 1
+    for (; i<srcArray.length; i++) {
+        r = fn(srcArray[i], r)
+    }
+
+    return r;
+}
